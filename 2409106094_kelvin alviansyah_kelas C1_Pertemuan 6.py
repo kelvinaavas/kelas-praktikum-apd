dffff51dd370294e7663ae94cdf4c83b8903bca9
kelvin alviansyah <104289852+kelvinaavas@users.noreply.github.com>
2024-09-25 16:14:05 +0800

    Add files via upload

---
+++ 2409106094_kelvin alviansyah_kelas C1_Pertemuan 6.py
@@ -0,0 +1,64 @@
+# ulang =10
+# for i in range (ulang):
+#     print("hallo")
+
+# simpan = [12, "udin petot", 14.5, True, 'a',"102"]
+# for i in simpan[2:5]:
+#     print(i)
+
+# makanan = ["mie", "sop", "bakso"]
+# minuman = ["es teh", "air putih", "es jeruk"]
+
+# for i in makanan:
+#     for j in minuman:
+#         print(f"{i} & {j}")
+
+# print("Menu Rumah Makan Informatika 24: ")
+# print("---------------------------")
+# simpan = ["Nasi Goreng", "Mle Goreng", "Mie Ayam", "Bakso", "Soto"]
+# for i in simpan:
+#     print(i)
+
+# print("Menu Rumah Makan Informatika 24: ")
+# print("---------------------------")
+# simpan = ["Nasi Goreng", "Mle Goreng", "Mie Ayam", "Bakso", "Soto"]
+# for i, menu in enumerate(simpan,start=100):
+#     print(f"{i}.{menu}")
+
+# print("Menu Rumah Makan Informatika 24: ")
+# print("---------------------------")
+# simpan = ["Nasi Goreng", "Mle Goreng", "Mie Ayam", "Bakso", "Soto"]
+# for i in range(len(simpan)):
+#     print(f"{i+1}. {simpan[i]}")
+
+# jawab = 'ya'
+# hitung = 0
+# while(jawab == 'ya'):
+#   hitung += 1
+#   jawab = input("Ulang lagi tidak? ")
+# print(f"Total perulangan: {hitung}")
+
+
+# jawab = 'ya'
+# hitung = 0
+# while(True):
+#     hitung += 1
+#     jawab = input("balikan lagi tidak ?")
+#     if jawab == "ga" or jawab == "engga" :
+#         prin(f"total perulangan: {hitung}")
+#     else :
+#       break
+
+# print("Daftar bilangan ganjil dari 1-10*")
+# for i in range(10):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+# hitung = 0
+# while True:
+#     hitung += 1
+#     ulang = input ("Masth Ingin Lanjut ? ")
+#     if ulang == "y" or ulang =="Y":
+#         print("oke kita Lanjut")
+#         continue
